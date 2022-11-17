899934bfdd35a5a98f65360d9f507a02c1214e28
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-17 11:17:01 -0600

    Add name from review userId

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,9 @@
-const Router = require('express').Router()
-const controller = require('../controllers/UserController')
+const Router = require("express").Router();
+const controller = require("../controllers/UserController");
 
-Router.get('/', controller.getUsers)
-Router.get('/:user_email', controller.getUserByEmail)
-Router.put('/', controller.updateUserEmail)
+Router.get("/", controller.getUsers);
+Router.get("/:user_id", controller.getOneUser);
+Router.get("/email/:user_email", controller.getUserByEmail);
+Router.put("/", controller.updateUserEmail);
 
-module.exports = Router
+module.exports = Router;

162e4a202198d813740969a08f492948f57b5e0f
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-17 17:28:53 -0600

    Add leave bootcamp functionality

--- controllers/UserBootcampsController.js
+++ controllers/UserBootcampsController.js
@@ -24,6 +24,18 @@ const getOneUserBootcamp = async (req, res) => {
   }
 };
 
+const getUserJoinedBootcamp = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id);
+    const userBootcampList = await UserBootcamp.findAll({
+      where: { userId: userId }
+    });
+    res.send(userBootcampList);
+  } catch (error) {
+    throw error;
+  }
+};
+
 const joinBootcamp = async (req, res) => {
   try {
     let bootcampId = parseInt(req.params.bootcamp_id);
@@ -40,8 +52,31 @@ const joinBootcamp = async (req, res) => {
   }
 };
 
+const leaveBootcamp = async (req, res) => {
+  try {
+    let bootcampId = parseInt(req.params.bootcamp_id);
+    const token = req.headers["authorization"].split(" ")[1];
+    let payload = jwt.verify(token, APP_SECRET);
+    const bootcamp = await UserBootcamp.findOne({
+      where: { userId: payload.id, bootcampId: bootcampId }
+    });
+    if (bootcamp) {
+      await UserBootcamp.destroy({
+        where: { bootcampId: bootcampId, userId: payload.id }
+      });
+      res.send({ message: "User succesfully leave the bootcamp" });
+    } else {
+      res.send({ message: "User is not the member of the bootcamp" });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   getUserBootcamps,
   getOneUserBootcamp,
-  joinBootcamp
+  getUserJoinedBootcamp,
+  joinBootcamp,
+  leaveBootcamp
 };
--- routes/UserBootcampsRouter.js
+++ routes/UserBootcampsRouter.js
@@ -1,7 +1,15 @@
 const Router = require("express").Router();
 const controller = require("../controllers/UserBootcampsController");
+const middleware = require("../middleware");
 
 Router.get("/", controller.getUserBootcamps);
-Router.post("/:bootcamp_id", controller.joinBootcamp);
+Router.post(
+  "/:bootcamp_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.joinBootcamp
+);
+Router.get("/:user_id", controller.getUserJoinedBootcamp);
+Router.delete("/:bootcamp_id", controller.leaveBootcamp);
 
 module.exports = Router;
